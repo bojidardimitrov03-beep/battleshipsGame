02da30fec11808d1c9492a7a5410ada7129fb481
Bojidar <254455568+bojidardimitrov03-beep@users.noreply.github.com>
2026-02-22 17:12:13 +0200

    add player class with tests

--- scr/Player.js
+++ scr/Player.js
@@ -1,12 +1,24 @@
 import Gameboard from "./Gameboard";
 
-class Player{
-    constructor(){
-        this.gameboard = new Gameboard;
-    }
-    attack(enemyGameboard,[row,col]){
-        enemyGameboard.receiveAttack([row,col])
-    }
+class Player {
+  constructor(type = "human") {
+    this.type = type;
+    this.attackedCoordinates = [];
+    this.gameboard = new Gameboard();
+  }
+  attack(enemyGameboard, [row, col]) {
+    enemyGameboard.receiveAttack([row, col]);
+  }
+  computerAttack(enemyGameboard) {
+    let row, col;
+    do {
+      row = Math.floor(Math.random() * 10);
+      col = Math.floor(Math.random() * 10);
+    } while (this.attackedCoordinates.some(([r, c]) => r === row && c === col));
+
+    this.attackedCoordinates.push([row, col]);
+    enemyGameboard.receiveAttack([row, col]);
+  }
 }
 
-export default Player
+export default Player;
--- scr/Player.test.js
+++ scr/Player.test.js
@@ -1,16 +1,26 @@
-import Player from './Player';
-import Gameboard from './Gameboard';
+import Gameboard from "./Gameboard";
+import Player from "./Player";
 
-describe('Player', () => {
-  test('has its own gameboard', () => {
+describe("Player", () => {
+  test("has its own gameboard", () => {
     const player = new Player();
     expect(player.gameboard).toBeInstanceOf(Gameboard);
   });
 });
 
-test('attack sends receiveAttack to enemy gameboard', () => {
-    const player1 = new Player();
-    const player2 = new Player();
-    player1.attack(player2.gameboard, [0, 0]);
-    expect(player2.gameboard.missedAttacks).toEqual([[0, 0]]);
-  });
+test("attack sends receiveAttack to enemy gameboard", () => {
+  const player1 = new Player();
+  const player2 = new Player();
+  player1.attack(player2.gameboard, [0, 0]);
+  expect(player2.gameboard.missedAttacks).toEqual([[0, 0]]);
+});
+test("never attacks the same coordinate twice", () => {
+  const player1 = new Player();
+  const computer = new Player("computer");
+  computer.computerAttack(player1.gameboard);
+  computer.computerAttack(player1.gameboard);
+  computer.computerAttack(player1.gameboard);
+  const missedAttacks = player1.gameboard.missedAttacks;
+  const unique = new Set(missedAttacks.map((coord) => coord.toString()));
+  expect(unique.size).toBe(3);
+});
